2c90eb6475849619e45be84752b2064600de8e80
Aleksandar Mihajlovski <a.mihajlovski91@gmail.com>
2016-10-21 01:27:13 +0200

    get keyword from command line

--- app.js
+++ app.js
@@ -16,24 +16,18 @@ app.listen(port);
 console.log('Executing script...');
 
 var computerUserName = process.env['USERPROFILE'].split(path.sep)[2];
-
-var skypeUserName = '';
-if(typeof process.argv[2] === 'undefined'){
-  console.log('Enter skype username and start the script again');
-} else {
-  skypeUserName = process.argv[2];
-}
-
+var skypeUserName = typeof process.argv[2] !== 'undefined' ? process.argv[2] : '';
 var skypeDbPath = path.join('C:', 'Users', computerUserName, 'AppData', 'Roaming', 'skype', skypeUserName, 'main.db');
+var keyword = typeof process.argv[3] !== 'undefined' ? process.argv[3] : '#pcmkfood';
 
 fs.stat(skypeDbPath, function(err, stat){
   if(err == null) {
-        console.log('Valid Skype user...');
-        getOrders(skypeDbPath);
+      console.log('Valid Skype user...');
+      getOrders(skypeDbPath);
     } else if(err.code == 'ENOENT') {
-        console.log('Invalid Skype user!');
+      console.log('Invalid Skype user!');
     } else {
-        console.log('Unknown error: ', err.code);
+      console.log('Unknown error: ', err.code);
     }
     process.exit();
 });
@@ -42,17 +36,17 @@ function getOrders(skypeDbPath){
   var filebuffer = fs.readFileSync(skypeDbPath);
   var db = new SQL.Database(filebuffer);
   var startOfDay = new Date().setHours(0,0,0,0);
-  var query = "SELECT from_dispname, body_xml, timestamp__ms FROM Messages WHERE timestamp__ms > " + startOfDay + " AND body_xml LIKE '%asdasdasdasd%'";
+  var query = "SELECT from_dispname, body_xml, timestamp__ms FROM Messages WHERE timestamp__ms > " + startOfDay + " AND body_xml LIKE '%" + keyword + "%'";
   console.log('Executing query...');
   var res = db.exec(query);
   if(res.length){
     var results = res[0].values;
     console.log('================================================');
-    for(var i = 0, order; order = results[i]; i++){
-      console.log(order[0] + ' ' + order[1]);
+    for(var i = 0, item; item = results[i]; i++){
+      console.log(i + 1 + '.' + item[0] + ' ' + item[1].replace(keyword, '') + '|');
     }
     console.log('================================================');
-    console.log('Participants: ' + results.length);
+    console.log('Number of results: ' + results.length);
     console.log('================================================');
   } else {
     console.log('No results...');
